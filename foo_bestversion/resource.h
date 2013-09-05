df9d4bbac5985178e4635b92985acfe27ec0aae9
Ben Hymers <b.hymers@gmail.com>
2013-09-05 16:28:25 +0100

    Removed some old unused IDs from resource.h

--- foo_bestversion/resource.h
+++ foo_bestversion/resource.h
@@ -2,36 +2,13 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDCANCEL3                       6
-#define IDD_DSP                         101
-#define IDD_BANDPASS1                   107
 #define IDD_TEXT_TO_PLAYLIST_DIALOGUE   110
-#define IDD_NORCH                       112
-#define IDC_BANDPASSQINFO2              1033
-#define IDC_WAHLFOFREQINFO2             1037
-#define IDC_DISPLAY                     1040
-#define IDC_PEAKLIMIT                   1042
-#define IDD_TEMPO1                      1045
-#define IDC_RELEASETIME                 1047
-#define IDC_IIRQ                        1049
-#define IDC_FASTRATIO                   1050
-#define IDC_IIRQINFO                    1052
-#define IDC_SLOWRATIO                   1053
-#define IDC_GAIN                        1056
-#define IDC_DISPLAY_GAIN                1059
-#define IDC_DISPLAY_PEAK                1061
-#define IDC_DISPLAY_RELEASE             1064
 #define IDC_INPUT_TEXT                  1064
 #define IDC_OUTPUT_TEXT                 1065
 #define IDC_ARTIST_REGEX_TEXT           1066
-#define IDC_DISPLAY_FASTRATIO           1067
-#define IDC_FORMAT_STRING_LABEL         1067
 #define IDC_ARTIST_REGEX_LABEL          1067
 #define IDC_TITLE_REGEX_TEXT            1068
 #define IDC_TITLE_REGEX_LABEL           1069
-#define IDC_DISPLAY_SLOWRATIO           1070
-#define IDC_COMBO2                      1077
-#define IDC_SLIDER                      -1
 
 // Next default values for new objects
 // 
