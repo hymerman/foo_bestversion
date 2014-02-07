713a3e214467ca4a88092ac71f17d7943aaec8fe
Ben Hymers <b.hymers@gmail.com>
2014-02-07 20:52:48 +0000

    Set version to 1.0.0

--- foo_bestversion/Component.h
+++ foo_bestversion/Component.h
@@ -2,7 +2,7 @@
 
 #define COMPONENT_NAME "Best Version Picker"
 #define COMPONENT_FILENAME "foo_bestversion.dll"
-#define COMPONENT_VERSION "0.1"
+#define COMPONENT_VERSION "1.0.0"
 #define COMPONENT_ABOUT_MESSAGE\
 	"Picks the best version of a track from the library.\n\n"\
 	"Also grabs an artist's top tracks feed from last.fm,\n"\
