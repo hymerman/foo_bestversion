f81d03ebd083fc5b02e369ff4590584126203470
Ben Hymers <b.hymers@gmail.com>
2016-08-05 22:55:02 +0100

    Automated changes to resource files

--- foo_bestversion/resource.h
+++ foo_bestversion/resource.h
@@ -12,14 +12,15 @@
 #define IDC_TITLE_INDEX_TEXT            1070
 #define IDC_TITLE_INDEX_LABEL           1071
 #define IDC_STATUS_TEXT                 1072
+#define IDC_COMBO1                      1073
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        111
+#define _APS_NEXT_RESOURCE_VALUE        112
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1073
+#define _APS_NEXT_CONTROL_VALUE         1074
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
